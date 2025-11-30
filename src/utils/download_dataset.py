095a159989b2eea1c2400b4c84ee9f7376538637
João Victor M. B. da Silva <120438464+Jvfrostbr@users.noreply.github.com>
2025-11-30 05:54:54 -0300

    delete files

--- src/utils/download_dataset.py
+++
@@ -1,8 +0,0 @@
-from datasets import load_dataset
-
-# carrega automaticamente do cache local,
-# e baixa só se ainda não estiver no cache
-dataset = load_dataset("Hemg/AI-Generated-vs-Real-Images-Datasets")
-
-print(dataset)
-print(dataset["train"][0])
